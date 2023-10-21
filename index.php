d231bf42539195b4551620dfc4771a12122f6c45
MoizSethi <m.moizsethi@gmail.com>
2023-10-21 11:55:09 -0700

    main

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Theme/style.css">
+    <title>FFS</title>
+</head>
+<body>
+    <header>
+        <img src="asset/logo.png" alt="Logo" class="logo" width="150px">
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav>
+        <button class="btn">Login</button>
+    </header>
+    <?php include 'pages/main.php'; ?>
+    <footer>
+        <p>&copy; 2021 FFS | All Right Reserved.</p>
+    </footer>
+</body>
+</html>
